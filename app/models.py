98e33b83f4231201d32d073bb136157852433fac
Kuno Claes(D) <kuno@icodes.dev>
2023-11-07 23:16:23 +0100

    Converted to mysql

--- app/models.py
+++ app/models.py
@@ -5,6 +5,6 @@ class Book(Base):
     __tablename__ = 'books'
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True)
-    author = Column(String, index=True)
-    genre = Column(String, index=True)
+    title = Column(String(25), index=True)
+    author = Column(String(25), index=True)
+    genre = Column(String(25), index=True)
